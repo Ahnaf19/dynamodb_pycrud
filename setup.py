9a82c950451ec97ede92cbf01a3f2889ff4513da
Ahnaf19 <ahnaftanjid19@gmail.com>
2024-11-19 18:32:56 +0600

    del .toml edit setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,16 @@ setup(
     author_email="ahnaftanjid19@gmail.com",
     url="https://github.com/Ahnaf19/dynamodb_pycrud.git",
     packages=find_packages(),  # Automatically find package directories
-    install_requires=["boto3>=1.28.0", "botocore>=1.28.0"],  # Specify boto3 dependency
+    install_requires=[  # Specify dependency
+        "boto3==1.35.57",
+        "botocore==1.35.57",
+        "jmespath==1.0.1",
+        "s3transfer==0.10.3",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
